745f8de42942b5cdab091cdcc5a33c5eddfe86b3
felixjtdb <felixjtdb@gmail.com>
2018-11-29 15:20:06 +0000

    change to registration contorller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class RegistrationsController < Devise::RegistrationsController
+  extend Devise
   devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :trackable,
        :validatable, authentication_keys: [:login, :sign_up]
