34c1d864ba05393cb9f07245e3875ecd94c224c0
felixjtdb <felixjtdb@gmail.com>
2018-11-29 16:19:40 +0000

    putting test back

--- spec/features/can_view_other_timelines_spec.rb
+++ spec/features/can_view_other_timelines_spec.rb
@@ -15,4 +15,16 @@ describe "Other timelines" do
     expect(page).to have_content("Sign Up")
 
   end
+
+  it "can view other users timelines when logged in" do
+    visit_site_sign_up
+    url = current_url.split('/')
+    id = url[3]
+    visit '/'
+    sign_out
+    sign_up_other_user
+    visit "/#{id}"
+
+    expect(page).to have_content("username123's wall")
+  end
 end
